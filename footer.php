4fafef958b6b527ecd38e0e8e604a3b3082faf2b
stilki4a <stilki4a@abv.bg>
2017-02-28 14:44:45 +0200

    headerS

--- footer.php
+++ footer.php
@@ -1,9 +1,8 @@
-<?php
-?>
+</div>
 <footer id="footer">
-	<a href=""> Общи условия</a>
-	<a href=""> За нас</a>
-	<a href=""> Контакти</a>
+    <a href=""> Общи условия</a>
+    <a href=""> За нас</a>
+    <a href=""> Контакти</a>
 </footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <title>AllStuff.bg</title>
     <link rel="stylesheet" href="./assets/css/ressets.css" type="text/css" />
     <link rel="stylesheet" href="./assets/css/style.css" type="text/css" />
@@ -10,35 +10,32 @@
 
 </head>
 <body>
-	<div id="wrapper">
-		<header>
-		
-		<div id="logo">
-			<h2><a href="./index.php">AllStuff.bg</a></h2>
-			
-			
-				<div class="buton" id="obqva">
-				
-				
-					<a  href="./newes.php">Добави обява</a>
-					<input type="image" src="./assets/images/plus2.png" alt="Submit">
-				
-			</div>	
-				<div class="buton">
-				
-					<a  href="./registraton.php">Моят профил</a>
-				</div>
-			
-		</div>
-		
-	    <div id="underhome">
-		
-		<div id="search">
-			<input type="text" name="search" value="Search..." size="150">
-			<input type="image" src="./assets/images/lupa2.png" alt="Submit">
-		</div>
+<div id="wrapper">
+    <header>
+
+        <div id="logo">
+            <h2><a href="./index.php">AllStuff.bg</a></h2>
+
+
+            <div class="buton" id="obqva">
+
+
+                <a  href="./newes.php">Добави обява</a>
+                <input type="image" src="./assets/images/plus2.png" alt="Submit">
+
+            </div>
+            <div class="buton">
+
+                <a  href="./registraton.php">Моят профил</a>
+            </div>
+
         </div>
-		</header>
 
+        <div id="underhome">
 
-
+            <div id="search">
+                <input type="text" name="search" value="Search..." size="150">
+                <input type="image" src="./assets/images/lupa2.png" alt="Submit">
+            </div>
+        </div>
+    </header>
