0b99c1babecb559f5385a96fd246ad08d2f2f201
stilki4a <stilki4a@abv.bg>
2017-03-03 17:33:45 +0200

    pic

--- cars.php
+++ cars.php
@@ -1,19 +1,19 @@
 
 <div border="1px" solid black id="avtowrapper">
     <div class="avtomobili">
-        <img src="../FCAllStuff/assets/images/Audi-Logo-PNG.png" alt="audilogo">
+        <img src="../allStuff/assets/images/Audi-Logo-PNG.png" alt="audilogo">
         <h3><a href="">Audi</a></h3>
         <p class="date">data</p>
         <p class="suma">cena</p>
     </div>
     <div class="avtomobili">
-        <img src="../FCAllStuff/assets/images/bmw.jpg" alt="bmw">
+        <img src="../allStuff/assets/images/bmw.jpg" alt="bmw">
         <h3> <a href="">Audi</a></h3>
         <p class="date">data</p>
         <p class="suma">cena</p>
     </div>
     <div class="avtomobili">
-        <img src="../FCAllStuff/assets/images/mercedes-benz-logo.png" alt="mercedes">
+        <img src="../allStuff/assets/images/mercedes-benz-logo.png" alt="mercedes">
         <h3><a href="">Audi</a></h3>
         <p class="date">data</p>
         <p class="suma">cena</p>
