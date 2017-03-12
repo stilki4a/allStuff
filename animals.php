9f1725bea792caba5b1b488191613e14c2459c55
stilki4a <stilki4a@abv.bg>
2017-03-12 15:24:30 +0200

    bmw

--- animals.php
+++ animals.php
@@ -1,19 +1,19 @@
 
 <div border="1px" solid black id="avtowrapper">
     <div class="avtomobili">
-        <img src="../allStuff/assets/images/Audi-Logo-PNG.png" alt="audilogo">
+        <img src="../allStuff/assets/images/agne.png" alt="agne">
         <h3><a href="">Audi</a></h3>
         <p class="date">data</p>
         <p class="suma">cena</p>
     </div>
     <div class="avtomobili">
-        <img src="../allStuff/assets/images/bmw.jpg" alt="bmw">
+        <img src="../allStuff/assets/images/jaba.png" alt="jaba">
         <h3> <a href="">Audi</a></h3>
         <p class="date">data</p>
         <p class="suma">cena</p>
     </div>
     <div class="avtomobili">
-        <img src="../allStuff/assets/images/mercedes-benz-logo.png" alt="mercedes">
+        <img src="../allStuff/assets/images/tom_i_djeri.jpg" alt="tomidjeri">
         <h3><a href="">Audi</a></h3>
         <p class="date">data</p>
         <p class="suma">cena</p>
--- cars.php
+++ cars.php
@@ -7,8 +7,8 @@
         <p class="suma">cena</p>
     </div>
     <div class="avtomobili">
-        <img src="../allStuff/assets/images/bmw.jpg" alt="bmw">
-        <h3> <a href="">Audi</a></h3>
+        <a href="bmw.php"><img src="../allStuff/assets/images/bmw.jpg" alt="bmw"></a>
+        <h3> <a href="bmw.php">bmw</a></h3>
         <p class="date">data</p>
         <p class="suma">cena</p>
     </div>
