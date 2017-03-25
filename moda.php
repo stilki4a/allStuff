de2c8e440264588b14ea96406d532c09de98df3a
stilki4a <stilki4a@abv.bg>
2017-03-25 18:55:18 +0200

    OK

--- moda.php
+++ moda.php
@@ -1,20 +1,11 @@
 
 <div class="obsti">
     <?php
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'all_stuff');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
 
     try {
         $db = new PDO ("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-//                $pstmt = $db->prepare("Select o.obqva_id,o.obqva_zagl,o.price,l.location_name,o.picture_name
-//                                             From obqva o
-//                                             JOIN locations l
-//                                             ON l.location_id = o.fk_location_id");
         $pstmt = $db ->prepare("SELECT obqva_id,obqva_name,picture_name,price FROM obqva WHERE fk_cat_id =3");
 
 
--- proba.php
+++ proba.php
@@ -8,10 +8,6 @@ if (isset($_GET['name'])) {
     $obCena = "";
     $obLokacia = "";
 
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'all_stuff');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
 
     try {
         $db = new PDO ("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
